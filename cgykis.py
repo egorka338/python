596158ea25ca92d4bb81a1f537c0fcd5a2dd66f5
egor <egor.sedov.11@mail.ru>
2025-10-11 18:43:24 +0300

    added new files

--- cgykis.py
+++ cgykis.py
@@ -2,10 +2,11 @@ a = int(input('введите шестизначное число'))
 b = a // 100000
 c = a // 10000 % 10
 d = a // 1000 % 10
-t = a // 10000 % 10
-s = a // 10000 % 10
+t = a // 100 % 10
+s = a // 10 % 10
 o = a % 10
 if b + c + d == t + s + o:
     print('happy')
 else:
     print('unhappy')
+print(b,c,d,t,s,o)
--- xaxaxaxaxaxa.py
+++ xaxaxaxaxaxa.py
@@ -2,7 +2,7 @@ a = int(input('введите трехзначное число'))
 b = a // 100
 c = a // 10 % 10
 h = a % 10
-if (b + c + h) % 5 == int :
+if (b + c + h) % 5 == 0:
     print('yes')
 else:
     print('no')
